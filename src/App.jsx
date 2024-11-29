5f7cda1214b1de19945c6b15452ee2de8826cda8
Ankit Mishra <aktmishra72@gmail.com>
2024-11-29 20:19:50 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-
-
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./features/counter/counterSlice";
+import { increment, selectCount } from "./features/counter/counterSlice";
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
+  const count = useSelector(selectCount);
   const dispatch = useDispatch();
 
   return (
     <>
       <div className="flex flex-col gap-5 items-center ">
         <p>{count}</p>
-        <div className="flex gap-5 "><button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>decrement</button></div>
+        <div className="flex gap-5 ">
+          <button onClick={() => dispatch(increment())}>Increment</button>
+         
+        </div>
       </div>
     </>
   );
