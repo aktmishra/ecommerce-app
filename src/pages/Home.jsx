326f233da9e98e3aa75be654c2676b9099d7fd7e
Ankit Mishra <aktmishra72@gmail.com>
2024-11-30 18:13:45 +0530

    Login and Signup component and LoginPage SignupPage added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "../features/navbar/Navbar";
 import ProductList from "../features/product-list/ProductList";
+import LoginPage from "./LoginPage";
+ 
+import SignupPage from "./SignupPage";
 
 function Home() {
   return (
     <div>
-      <Navbar>
+      {/* <Navbar>
         <ProductList></ProductList>
-      </Navbar>
+      </Navbar> */}
+      {/* <LoginPage></LoginPage> */}
+      <SignupPage></SignupPage>
     </div>
   );
 }
